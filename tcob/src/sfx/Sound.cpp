2e8682b00dd8de66c1facbcca2f5d627961ae632
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2021-05-25 08:42:25 +0200

    important changes

--- tcob/src/sfx/Sound.cpp
+++ tcob/src/sfx/Sound.cpp
@@ -12,36 +12,10 @@
 #define DR_WAV_NO_STDIO
 #include <dr_libs/dr_wav.h>
 
+#include "AudioIO.hpp"
 #include <tcob/core/io/FileStream.hpp>
 
 namespace tcob {
-
-auto read(void* userdata, void* buffer, isize bytesToRead) -> isize
-{
-    InputFileStream* stream { reinterpret_cast<InputFileStream*>(userdata) };
-    return stream->read(reinterpret_cast<byte*>(buffer), bytesToRead);
-}
-
-auto seek_wav(void* userdata, i32 offset, drwav_seek_origin origin) -> u32
-{
-    InputFileStream* stream { reinterpret_cast<InputFileStream*>(userdata) };
-    auto dir { origin == drwav_seek_origin_current ? std::ios_base::cur : std::ios_base::beg };
-    return static_cast<u32>(stream->seek(offset, dir));
-}
-
-auto seek_flac(void* userdata, i32 offset, drflac_seek_origin origin) -> u32
-{
-    InputFileStream* stream { reinterpret_cast<InputFileStream*>(userdata) };
-    auto dir { origin == drflac_seek_origin_current ? std::ios_base::cur : std::ios_base::beg };
-    return static_cast<u32>(stream->seek(offset, dir));
-}
-
-auto seek_mp3(void* userdata, i32 offset, drmp3_seek_origin origin) -> u32
-{
-    InputFileStream* stream { reinterpret_cast<InputFileStream*>(userdata) };
-    auto dir { origin == drmp3_seek_origin_current ? std::ios_base::cur : std::ios_base::beg };
-    return static_cast<u32>(stream->seek(offset, dir));
-}
 
 Sound::Sound()
 {
@@ -78,7 +52,7 @@ auto Sound::load(const std::string& filename) -> bool
         if (ext == ".wav") {
             u64 frameCount { 0 };
             u32 channels { 0 }, sampleRate { 0 };
-            auto audioData { drwav_open_and_read_pcm_frames_s16(&read, &seek_wav, &stream, &channels, &sampleRate, &frameCount, nullptr) };
+            auto audioData { drwav_open_and_read_pcm_frames_s16(&detail::read, &detail::seek_wav, &stream, &channels, &sampleRate, &frameCount, nullptr) };
             if (audioData) {
                 _buffer->buffer_data(channels, audioData, static_cast<i32>(frameCount * 2), sampleRate);
                 drwav_free(audioData, nullptr);
@@ -87,7 +61,7 @@ auto Sound::load(const std::string& filename) -> bool
         } else if (ext == ".flac") {
             u64 frameCount { 0 };
             u32 channels { 0 }, sampleRate { 0 };
-            auto audioData { drflac_open_and_read_pcm_frames_s16(&read, &seek_flac, &stream, &channels, &sampleRate, &frameCount, nullptr) };
+            auto audioData { drflac_open_and_read_pcm_frames_s16(&detail::read, &detail::seek_flac, &stream, &channels, &sampleRate, &frameCount, nullptr) };
             if (audioData) {
                 _buffer->buffer_data(channels, audioData, static_cast<i32>(frameCount * 2), sampleRate);
                 drflac_free(audioData, nullptr);
@@ -96,7 +70,7 @@ auto Sound::load(const std::string& filename) -> bool
         } else if (ext == ".mp3") {
             u64 frameCount { 0 };
             drmp3_config config {};
-            auto audioData { drmp3_open_and_read_pcm_frames_s16(&read, &seek_mp3, &stream, &config, &frameCount, nullptr) };
+            auto audioData { drmp3_open_and_read_pcm_frames_s16(&detail::read, &detail::seek_mp3, &stream, &config, &frameCount, nullptr) };
             if (audioData) {
                 _buffer->buffer_data(config.channels, audioData, static_cast<i32>(frameCount * 2), config.sampleRate);
                 drmp3_free(audioData, nullptr);
