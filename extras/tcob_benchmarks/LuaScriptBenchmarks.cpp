5d1e7285208df36c054ebd55ef013e39eabd0834
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-03-15 09:25:04 +0100

    fixed benchmarks

--- extras/tcob_benchmarks/LuaScriptBenchmarks.cpp
+++ extras/tcob_benchmarks/LuaScriptBenchmarks.cpp
@@ -62,7 +62,7 @@ void static LUA_GetAndCallFunction(benchmark::State& state)
     script                s;
     [[maybe_unused]] auto res = s.run("function foo() return 100 end");
     for (auto _ : state) {
-        tcob::scripting::lua::function<int> f = s.get_global_table()["foo"];
+        tcob::scripting::lua::function<int> f {s.get_global_table()["foo"]};
         auto                                x = f();
         benchmark::DoNotOptimize(x);
     }
@@ -81,7 +81,7 @@ struct converter<p> {
     auto static IsType(state_view state, i32 idx) -> bool
     {
         if (state.is_table(idx)) {
-            table tab {state, idx};
+            table tab {table::Acquire(state, idx)};
             return (tab.is<f32>("x") && tab.is<f32>("y"));
         }
         return false;
@@ -90,10 +90,10 @@ struct converter<p> {
     auto static From(state_view state, i32& idx, p& value) -> bool
     {
         if (state.is_table(idx)) {
-            table tab {state, idx++};
+            table tab {table::Acquire(state, idx++)};
             if (tab.is<f32>("x") && tab.is<f32>("y")) {
-                value.x = tab["x"];
-                value.y = tab["y"];
+                value.x = tab["x"].as<f32>();
+                value.y = tab["y"].as<f32>();
                 return true;
             }
         }
@@ -103,7 +103,7 @@ struct converter<p> {
 
     void static To(state_view state, p const& value)
     {
-        table tab {table::CreateNew(state)};
+        table tab {state};
 
         tab["x"] = value.x;
         tab["y"] = value.y;
@@ -115,7 +115,7 @@ void static LUA_StructConverter(benchmark::State& state)
 {
     script                            s;
     [[maybe_unused]] auto             res = s.run("function foo() return { x=100, y=200 } end");
-    tcob::scripting::lua::function<p> f   = s.get_global_table()["foo"];
+    tcob::scripting::lua::function<p> f {s.get_global_table()["foo"]};
 
     for (auto _ : state) {
         p    test {f()};
@@ -129,8 +129,8 @@ BENCHMARK(LUA_StructConverter)->Iterations(1000000);
 void static LUA_WrapperConverter(benchmark::State& state)
 {
     script                               s;
-    [[maybe_unused]] auto                res  = s.run("function foo(p) p.x=100 p.y=200 end");
-    tcob::scripting::lua::function<void> f    = s.get_global_table()["foo"];
+    [[maybe_unused]] auto                res = s.run("function foo(p) p.x=100 p.y=200 end");
+    tcob::scripting::lua::function<void> f {s.get_global_table()["foo"]};
     auto                                 wrap = s.create_wrapper<p>("p");
     wrap->wrap_property<&p::x>("x");
     wrap->wrap_property<&p::y>("y");
