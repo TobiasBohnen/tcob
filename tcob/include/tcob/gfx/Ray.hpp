4206bc81fce917c05ff49bfefd71b40866453fc1
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-09-24 20:50:17 +0200

    ray: added circle and func intersection

--- tcob/include/tcob/gfx/Ray.hpp
+++ tcob/include/tcob/gfx/Ray.hpp
@@ -11,7 +11,6 @@
 #include "tcob/core/AngleUnits.hpp"
 #include "tcob/core/Point.hpp"
 #include "tcob/core/Rect.hpp"
-#include "tcob/gfx/Gfx.hpp"
 
 namespace tcob::gfx {
 
@@ -24,10 +23,17 @@ public:
 
     auto intersect_line(point_f a, point_f b) const -> std::optional<point_f>;
     auto intersect_rect(rect_f const& rect) const -> std::vector<point_f>;
-    auto intersect_polygon(polygon_span polygon) const -> std::vector<point_f>;
+    auto intersect_circle(point_f const& center, f32 radius) -> std::vector<point_f>;
+
+    auto intersect_polygon(auto&& polygon) const -> std::vector<point_f>;
+    auto intersect_function(auto&& func, f64 tolerance = 0.01) const -> std::vector<point_f>;
 
 private:
+    auto intersect_segment(point_d const& rd, point_d const& p0, point_d const& p1) const -> std::optional<f64>;
+
     auto get_direction() const -> std::optional<point_d>;
 };
 
 }
+
+#include "Ray.inl"
--- tcob/src/gfx/Ray.cpp
+++ tcob/src/gfx/Ray.cpp
@@ -15,28 +15,12 @@ ray::ray(point_f origin, degree_d direction)
 {
 }
 
-auto static IntersectsSegment(point_d const& ro, point_d const& rd, point_d const& p0, point_d const& p1) -> std::optional<f64>
-{
-    point_d const seg {p1 - p0};
-    point_d const segPerp {seg.Y, -seg.X};
-
-    f64 const denom {segPerp.dot(rd)};
-    if (std::abs(denom) < epsilon) { return std::nullopt; }
-
-    point_d const d {p0 - ro};
-    f64 const     distance {segPerp.dot(d) / denom};
-    f64 const     s {point_d {rd.Y, -rd.X}.dot(d) / denom};
-    if (distance >= 0.0 && s >= 0.0 && s <= 1.0) { return distance; }
-
-    return std::nullopt;
-}
-
 auto ray::intersect_line(point_f a, point_f b) const -> std::optional<point_f>
 {
     auto const r {get_direction()};
     if (!r) { return std::nullopt; }
 
-    if (auto distance {IntersectsSegment(point_d {Origin}, *r, point_d {a}, point_d {b})}) {
+    if (auto distance {intersect_segment(*r, point_d {a}, point_d {b})}) {
         return point_f {Origin + *r * *distance};
     }
     return std::nullopt;
@@ -59,28 +43,47 @@ auto ray::intersect_rect(rect_f const& rect) const -> std::vector<point_f>
     return retValue;
 }
 
-auto ray::intersect_polygon(polygon_span polygon) const -> std::vector<point_f>
+auto ray::intersect_circle(point_f const& center, f32 radius) -> std::vector<point_f>
 {
     auto const r {get_direction()};
     if (!r) { return {}; }
 
+    f64 const b {2.0 * ((Origin.X - center.X) * r->X + (Origin.Y - center.Y) * r->Y)};
+    f64 const c {((Origin.X - center.X) * (Origin.X - center.X)) + ((Origin.Y - center.Y) * (Origin.Y - center.Y)) - (radius * radius)};
+
+    f64 const discr {(b * b) - (4 * c)};
+    if (discr < 0) { return {}; }
+
+    f64 const sqrtDiscr {std::sqrt(discr)};
+    f64 const s1 {(-b - sqrtDiscr) / 2};
+    f64 const s2 {(-b + sqrtDiscr) / 2};
+
     std::vector<point_f> retValue;
-
-    usize const n {polygon.size()};
-    for (usize i {0}; i < n; ++i) {
-        if (auto distance {IntersectsSegment(point_d {Origin}, *r, point_d {polygon[i]}, point_d {polygon[(i + 1) % n]})}) {
-            retValue.emplace_back(Origin + *r * *distance);
-        }
-    }
-
+    if (s1 >= 0) { retValue.push_back(Origin + *r * s1); }
+    if (s2 >= 0 && s2 != s1) { retValue.push_back(Origin + *r * s2); }
     return retValue;
+}
+
+auto ray::intersect_segment(point_d const& rd, point_d const& p0, point_d const& p1) const -> std::optional<f64>
+{
+    point_d const ro {Origin};
+    point_d const seg {p1 - p0};
+    point_d const segPerp {seg.Y, -seg.X};
+
+    f64 const denom {segPerp.dot(rd)};
+    if (std::abs(denom) < epsilon) { return std::nullopt; }
+
+    point_d const d {p0 - ro};
+    f64 const     distance {segPerp.dot(d) / denom};
+    f64 const     s {point_d {rd.Y, -rd.X}.dot(d) / denom};
+    if (distance >= 0.0 && s >= 0.0 && s <= 1.0) { return distance; }
+
+    return std::nullopt;
 }
 
 auto ray::get_direction() const -> std::optional<point_d>
 {
-    radian_d const rad {Direction - degree_d {90}};
-    point_d        r {rad.cos(), rad.sin()};
-    r = r.as_normalized();
+    auto const r {point_d::FromDirection(Direction)};
     if (std::abs(r.X) < epsilon && std::abs(r.Y) < epsilon) {
         return std::nullopt; // Invalid ray direction
     }
