246a2aff3bed20a558edd4b5e50f9ab6ee36e4d5
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-10-04 16:09:25 +0200

    camera improvements

--- tcob/include/tcob/gfx/Camera.hpp
+++ tcob/include/tcob/gfx/Camera.hpp
@@ -7,6 +7,7 @@
 #include "tcob/tcob_config.hpp"
 
 #include "tcob/core/Point.hpp"
+#include "tcob/core/Property.hpp"
 #include "tcob/core/Rect.hpp"
 #include "tcob/core/Transform.hpp"
 
@@ -17,54 +18,45 @@ class TCOB_API camera final {
     friend auto constexpr operator==(camera const&, camera const&) -> bool;
 
 public:
+    camera();
+    camera(camera const& other) noexcept;
+    auto operator=(camera const& other) noexcept -> camera&;
+
+    prop<size_f>  Size;
+    prop<size_f>  Zoom;
+    prop<point_f> Position;
+    prop<point_f> Offset;
+
     u32 VisibilityMask {0xFFFFFFFF};
 
-    auto get_matrix() -> mat4;
+    auto get_matrix() const -> mat4;
     auto get_viewport() const -> rect_f;
-    auto get_transformed_viewport() -> rect_f;
-
-    // TODO: meh vvvvvv
-    auto get_size() const -> size_f;
-    void set_size(size_f size);
-
-    auto get_offset() const -> point_f;
-    void set_offset(point_f pos);
-
-    auto get_zoom() const -> size_f;
-    void set_zoom(size_f zoom);
-
-    auto get_position() const -> point_f;
-    void set_position(point_f pos);
-    // TODO: meh ^^^^^^
+    auto get_transformed_viewport() const -> rect_f;
 
     void move_by(point_f offset);
     void look_at(point_f position);
-    auto get_look_at() -> point_f;
+    auto get_look_at() const -> point_f;
     void zoom_by(size_f factor);
 
-    auto convert_world_to_screen(rect_f const& rect) -> rect_i;
-    auto convert_world_to_screen(point_f point) -> point_i;
+    auto convert_world_to_screen(rect_f const& rect) const -> rect_i;
+    auto convert_world_to_screen(point_f point) const -> point_i;
 
-    auto convert_screen_to_world(rect_i const& rect) -> rect_f;
-    auto convert_screen_to_world(point_i point) -> point_f;
+    auto convert_screen_to_world(rect_i const& rect) const -> rect_f;
+    auto convert_screen_to_world(point_i point) const -> point_f;
 
 private:
     void update_transform();
 
-    bool      _transformDirty {true};
     transform _transform {transform::Identity};
-    size_f    _targetSize {size_f::Zero};
-    size_f    _zoom {size_f::One};
-    point_f   _position {point_f::Zero};
-    point_f   _offset {point_f::Zero};
 };
 
 auto constexpr operator==(camera const& left, camera const& right) -> bool
 {
-    return left._targetSize == right._targetSize
-        && left._zoom == right._zoom
-        && left._position == right._position
-        && left._offset == right._offset;
+    return left.VisibilityMask == right.VisibilityMask
+        && left.Size == right.Size
+        && left.Zoom == right.Zoom
+        && left.Position == right.Position
+        && left.Offset == right.Offset;
 }
 
 }
--- tcob/src/gfx/Camera.cpp
+++ tcob/src/gfx/Camera.cpp
@@ -9,138 +9,100 @@
 
 namespace tcob::gfx {
 
-auto camera::get_matrix() -> mat4
+camera::camera()
 {
-    update_transform();
+    Size.Changed.connect([&](auto const&) { update_transform(); });
+    Zoom(size_f::One);
+    Zoom.Changed.connect([&](auto const&) { update_transform(); });
+    Position.Changed.connect([&](auto const&) { update_transform(); });
+}
+
+camera::camera(camera const& other) noexcept
+{
+    *this = other;
+}
+
+auto camera::operator=(camera const& other) noexcept -> camera&
+{
+    Size           = other.Size();
+    Zoom           = other.Zoom();
+    Position       = other.Position();
+    Offset         = other.Offset();
+    VisibilityMask = other.VisibilityMask;
+
+    return *this;
+}
+
+auto camera::get_matrix() const -> mat4
+{
     return _transform.as_matrix4();
 }
 
 auto camera::get_viewport() const -> rect_f
 {
-    return {_offset, _targetSize};
+    return {Offset, Size};
 }
 
-auto camera::get_transformed_viewport() -> rect_f
+auto camera::get_transformed_viewport() const -> rect_f
 {
     return convert_screen_to_world(rect_i {get_viewport()});
 }
 
-auto camera::get_size() const -> size_f
-{
-    return _targetSize;
-}
-
-void camera::set_size(size_f size)
-{
-    if (_targetSize != size) {
-        _targetSize     = size;
-        _transformDirty = true;
-    }
-}
-
-auto camera::get_offset() const -> point_f
-{
-    return _offset;
-}
-
-void camera::set_offset(point_f pos)
-{
-    _offset = pos;
-}
-
-auto camera::get_zoom() const -> size_f
-{
-    return _zoom;
-}
-
-void camera::set_zoom(size_f zoom)
-{
-    if (_zoom != zoom) {
-        _zoom           = zoom;
-        _transformDirty = true;
-    }
-}
-
 void camera::zoom_by(size_f factor)
 {
-    set_zoom(_zoom * factor);
-}
-
-auto camera::get_position() const -> point_f
-{
-    return _position;
-}
-
-void camera::set_position(point_f pos)
-{
-    if (_position != pos) {
-        _position       = pos;
-        _transformDirty = true;
-    }
+    Zoom = (Zoom() * factor);
 }
 
 void camera::move_by(point_f offset)
 {
-    set_position(_position + offset);
+    Position = (Position() + offset);
 }
 
 void camera::look_at(point_f pos)
 {
-    point_f const offset {get_transformed_viewport().get_local_center() * point_f {_zoom.Width, _zoom.Height}};
-    set_position(pos - offset);
+    point_f const offset {get_transformed_viewport().get_local_center() * point_f {Zoom->Width, Zoom->Height}};
+    Position = pos - offset;
 }
 
-auto camera::get_look_at() -> point_f
+auto camera::get_look_at() const -> point_f
 {
-    return _position + get_transformed_viewport().get_local_center() * point_f {_zoom.Width, _zoom.Height};
+    return Position() + get_transformed_viewport().get_local_center() * point_f {Zoom->Width, Zoom->Height};
 }
 
-auto camera::convert_world_to_screen(rect_f const& rect) -> rect_i
+auto camera::convert_world_to_screen(rect_f const& rect) const -> rect_i
 {
-    update_transform();
-
     point_f const tl {convert_world_to_screen(rect.top_left())};
     point_f const br {convert_world_to_screen(rect.bottom_right())};
 
     return rect_i::FromLTRB(static_cast<i32>(tl.X), static_cast<i32>(tl.Y), static_cast<i32>(br.X), static_cast<i32>(br.Y));
 }
 
-auto camera::convert_world_to_screen(point_f point) -> point_i
+auto camera::convert_world_to_screen(point_f point) const -> point_i
 {
-    update_transform();
-
-    return point_i {_transform * point + _offset};
+    return point_i {_transform * point + Offset()};
 }
 
-auto camera::convert_screen_to_world(rect_i const& rect) -> rect_f
+auto camera::convert_screen_to_world(rect_i const& rect) const -> rect_f
 {
-    update_transform();
-
     point_f const tl {convert_screen_to_world(rect.top_left())};
     point_f const br {convert_screen_to_world(rect.bottom_right())};
 
     return rect_f::FromLTRB(tl.X, tl.Y, br.X, br.Y);
 }
 
-auto camera::convert_screen_to_world(point_i point) -> point_f
+auto camera::convert_screen_to_world(point_i point) const -> point_f
 {
-    update_transform();
-
-    return _transform.as_inverted() * (point_f {point} - _offset);
+    return _transform.as_inverted() * (point_f {point} - Offset());
 }
 
 void camera::update_transform()
 {
-    if (_transformDirty) {
-        _transform.to_identity();
-        if (_zoom != size_f::One) {
-            _transform.scale_at(_zoom, {_targetSize.Width / 2, _targetSize.Height / 2});
-        }
-        if (_position != point_f::Zero) {
-            _transform.translate({-_position.X, -_position.Y});
-        }
-
-        _transformDirty = false;
+    _transform.to_identity();
+    if (Zoom != size_f::One) {
+        _transform.scale_at(Zoom, {Size->Width / 2, Size->Height / 2});
+    }
+    if (Position != point_f::Zero) {
+        _transform.translate({-Position->X, -Position->Y});
     }
 }
 }
