1e7d7e9a646560f885e86767a570a1150ba03d57
TobiasBohnen <1294616+TobiasBohnen@users.noreply.github.com>
2024-10-24 07:22:38 +0200

    fixed build

--- tcob/include/tcob/gfx/Font.hpp
+++ tcob/include/tcob/gfx/Font.hpp
@@ -110,18 +110,6 @@ public:
         auto operator==(font::style const& other) const -> bool = default;
     };
 
-    void Serialize(font::style const& v, auto&& s)
-    {
-        s["is_italic"] = v.IsItalic;
-        s["weight"]    = v.Weight;
-    }
-
-    auto Deserialize(font::style& v, auto&& s) -> bool
-    {
-        return s.try_get(v.IsItalic, "is_italic")
-            && s.try_get(v.Weight, "weight");
-    }
-
     ////////////////////////////////////////////////////////////
 
     font();
@@ -159,6 +147,18 @@ private:
 
     assets::manual_asset_ptr<texture> _texture {};
 };
+
+void Serialize(font::style const& v, auto&& s)
+{
+    s["is_italic"] = v.IsItalic;
+    s["weight"]    = v.Weight;
+}
+
+auto Deserialize(font::style& v, auto&& s) -> bool
+{
+    return s.try_get(v.IsItalic, "is_italic")
+        && s.try_get(v.Weight, "weight");
+}
 
 ////////////////////////////////////////////////////////////
 }
