af1a2c6f23f8aad131aa6f13d31de70850855292
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2025-09-19 12:35:47 +0200

    bsa fixes

--- tcob/src/audio/audio_codecs/AudioCodec_bsa.cpp
+++ tcob/src/audio/audio_codecs/AudioCodec_bsa.cpp
@@ -23,11 +23,13 @@ namespace tcob::audio::detail {
 
 constexpr std::array<byte, 3> SIGNATURE {'B', 'S', 'A'};
 constexpr std::streamsize     OFFSET {sizeof(SIGNATURE) + sizeof(u8) + sizeof(u32) + sizeof(u32)};
+using pcm_t = i16;
 
 void bsa_decoder::seek_from_start(milliseconds pos)
 {
-    f64 const offset {pos.count() / 1000 * _info.Specs.SampleRate / _info.Specs.Channels};
-    stream().seek(static_cast<std::streamoff>(offset + OFFSET), io::seek_dir::Begin);
+    f64 const samples {pos.count() / 1000.0 * _info.Specs.SampleRate * _info.Specs.Channels};
+    f64 const bytes {samples * sizeof(pcm_t)};
+    stream().seek(static_cast<std::streamoff>(bytes + OFFSET), io::seek_dir::Begin);
 }
 
 auto bsa_decoder::open() -> std::optional<buffer::information>
@@ -48,10 +50,10 @@ auto bsa_decoder::open() -> std::optional<buffer::information>
 
 auto bsa_decoder::decode(std::span<f32> outputSamples) -> isize
 {
-    std::vector<i16> buffer(outputSamples.size());
-    stream().read_to<i16>(buffer);
+    std::vector<pcm_t> buffer(outputSamples.size());
+    stream().read_to<pcm_t>(buffer);
     for (usize i {0}; i < outputSamples.size(); ++i) {
-        outputSamples[i] = static_cast<f32>(buffer[i]) / std::numeric_limits<i16>::max();
+        outputSamples[i] = static_cast<f32>(buffer[i]) / std::numeric_limits<pcm_t>::max();
     }
     return std::ssize(outputSamples);
 }
@@ -65,11 +67,11 @@ auto bsa_encoder::encode(std::span<f32 const> samples, buffer::information const
     out.write<u32, std::endian::little>(static_cast<u32>(info.FrameCount));
     out.write<u32, std::endian::little>(static_cast<u32>(info.Specs.SampleRate));
 
-    std::vector<i16> buffer(samples.size());
+    std::vector<pcm_t> buffer(samples.size());
     for (usize i {0}; i < samples.size(); ++i) {
-        buffer[i] = static_cast<i16>(samples[i] * std::numeric_limits<i16>::max());
+        buffer[i] = static_cast<pcm_t>(samples[i] * std::numeric_limits<pcm_t>::max());
     }
-    out.write<i16>(buffer);
+    out.write<pcm_t>(buffer);
 
     return true;
 }
