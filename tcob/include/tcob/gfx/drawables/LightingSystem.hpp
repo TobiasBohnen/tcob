a4c54521bce5b0a12f47370bfdaf018ae3e0b75d
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-01-26 18:42:49 +0100

    fix lighting_system

--- tcob/include/tcob/gfx/drawables/LightingSystem.hpp
+++ tcob/include/tcob/gfx/drawables/LightingSystem.hpp
@@ -163,8 +163,9 @@ private:
     bool _isDirty {false};
     bool _updateGeometry {false};
 
-    polygon_renderer _renderer {buffer_usage_hint::DynamicDraw};
-    geometry_store   _store;
+    polygon_renderer    _renderer {buffer_usage_hint::DynamicDraw};
+    std::vector<vertex> _verts;
+    std::vector<u32>    _inds;
 
     asset_owner_ptr<material> _material;
 
