ff1b051f8a9704771cce617e216c78305ab9a76f
Tobias Bohnen <1294616+TobiasBohnen@users.noreply.github.com>
2026-04-14 22:03:16 +0200

    cleanup

--- tcob/include/tcob/core/UserObject.hpp
+++ tcob/include/tcob/core/UserObject.hpp
@@ -7,6 +7,7 @@
 #include "tcob/tcob_config.hpp"
 
 #include <memory>
+#include <type_traits>
 #include <typeindex>
 #include <utility>
 
@@ -17,18 +18,28 @@ class TCOB_API user_object final {
 public:
     user_object() = default;
 
+    user_object(user_object const&)                        = default;
+    user_object(user_object&&) noexcept                    = default;
+    auto operator=(user_object const&) -> user_object&     = default;
+    auto operator=(user_object&&) noexcept -> user_object& = default;
+
     template <typename T>
-    auto get() const -> T* { return _type == typeid(T) ? static_cast<T*>(_data.get()) : nullptr; }
+        requires(!std::is_same_v<std::decay_t<T>, user_object>)
+    user_object(T&& value);
+
+    template <typename T>
+        requires(!std::is_same_v<std::decay_t<T>, user_object>)
+    auto operator=(T&& value) -> user_object&;
+
+    template <typename T>
+    auto get() const -> T*;
 
     auto has_value() const -> bool;
     auto type() const -> std::type_index;
     void reset();
 
     template <typename T>
-    static auto Create(std::shared_ptr<T> ptr) -> user_object { return {std::move(ptr), typeid(T)}; }
-
-    template <typename T>
-    static auto Make(auto&&... args) -> user_object { return {std::make_shared<T>(args...), typeid(T)}; }
+    static auto Make(auto&&... args) -> user_object;
 
 private:
     user_object(std::shared_ptr<void> ptr, std::type_index type);
@@ -36,5 +47,37 @@ private:
     std::shared_ptr<void> _data;
     std::type_index       _type {typeid(void)};
 };
+
+////////////////////////////////////////////////////////////
+
+template <typename T>
+    requires(!std::is_same_v<std::decay_t<T>, user_object>)
+inline user_object::user_object(T&& value)
+{
+    *this = std::forward<T>(value);
+}
+
+template <typename T>
+    requires(!std::is_same_v<std::decay_t<T>, user_object>)
+inline auto user_object::operator=(T&& value) -> user_object&
+{
+    using U = std::decay_t<T>;
+
+    _data = std::make_shared<U>(std::forward<T>(value));
+    _type = typeid(U);
+    return *this;
+}
+
+template <typename T>
+inline auto user_object::get() const -> T*
+{
+    return _type == typeid(T) ? static_cast<T*>(_data.get()) : nullptr;
+}
+
+template <typename T>
+inline auto user_object::Make(auto&&... args) -> user_object
+{
+    return {std::make_shared<T>(args...), typeid(T)};
+}
 
 }
